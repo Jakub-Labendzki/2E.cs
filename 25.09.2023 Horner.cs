9f9ec8cb987317d7a01d061dfc3604f7f3c82924
Jakub Łabendzki <113890308+Jakub-Labendzki@users.noreply.github.com>
2023-09-25 14:11:51 +0200

    Update 25.09.2023 Horner.cs

--- 25.09.2023 Horner.cs
+++ 25.09.2023 Horner.cs
@@ -15,3 +15,33 @@ int horner(int system, string liczba)
 int system = int.Parse(Console.ReadLine());
 string liczba =  Console.ReadLine();
 Console.WriteLine(horner(system,liczba));
+
+
+// konwersja bin - dec i dec - bin
+
+int bin2dec(string bin)
+{
+    double wynik = 0;
+    int potega = 0;
+    for (int i = bin.Length - 1; i >= 0; i--)
+    {
+        wynik = wynik + Math.Pow(2d,potega) * char.GetNumericValue(bin[i]);
+        potega++;
+    }
+    return (int)wynik;
+}
+
+string dec2bin(int dec)
+{
+    string wynik = "";
+    string reszta;
+    while (dec > 0)
+    {
+        reszta = Convert.ToString(dec % 2);
+        wynik = new string(reszta) + wynik;
+        dec = dec / 2;
+    }
+    return wynik;
+}
+int decik = int.Parse(Console.ReadLine());
+Console.WriteLine(dec2bin(decik));
